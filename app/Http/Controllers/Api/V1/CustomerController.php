be4c5959f3912c9a50223cb6fb30de6d3c7556b3
emmanueltechman <emmanueltechman@gmail.com>
2022-11-05 11:41:57 +0100

    filtering data part -1

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Services\v1\CustomerQuery;
 
 
 class CustomerController extends Controller
@@ -18,9 +19,18 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); //['column','operator', 'value']
+        if (count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+
+        } else{
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+
+        }
+        //Customer::where($queryItems)
     }
 
     /**
